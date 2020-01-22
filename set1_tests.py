1f39b5f52ff29ce781948720531a5645bd7264ca
Mike Merinov <ticolensic@users.noreply.github.com>
2020-01-22 22:20:28 +0300

    Remove wrong messages

--- set1_tests.py
+++ set1_tests.py
@@ -16,7 +16,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge1(self):
         if 1 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         string = Hex("49276d206b696c6c696e6720796f757220627261696e206c"
                      "696b65206120706f69736f6e6f7573206d757368726f6f6d")
         test_string = Base64("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBs"
@@ -27,7 +27,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge2(self):
         if 2 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         data = Hex("1c0111001f010100061a024b53535009181c")
         key = Hex("686974207468652062756c6c277320657965")
         answer = Hex("746865206b696420646f6e277420706c6179")
@@ -37,7 +37,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge3(self):
         if 3 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         test_string = Hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
         output = crack_sb_xor_v2(from_hex(test_string))
         answer = b"Cooking MC's like a pound of bacon"
@@ -45,7 +45,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge4(self):
         if 4 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         answer = b"Now that the party is jumping\n"
         with open("materials\\4.txt") as f:
             _input = list(map(from_hex, f.read().splitlines()))
@@ -54,7 +54,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge5(self):
         if 5 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         answer1 = Hex("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272")
         answer2 = Hex("a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
         key = b"ICE"
@@ -65,7 +65,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge6(self):
         if 6 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected_output = b'Terminator X: Bring the noise'
 
         with open("materials\\6.txt") as f:
@@ -78,7 +78,7 @@ class TestSet1(unittest.TestCase):
     def test_challenge7(self):
         expected = b"I'm back and I'm ringin"
         if 7 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         key = b"YELLOW SUBMARINE"
         with open("materials\\7.txt") as f:
             _input = Base64(f.read())
@@ -88,7 +88,7 @@ class TestSet1(unittest.TestCase):
 
     def test_challenge8(self):
         if 8 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         answer = Hex(
             "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744c"
             "d283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c"
@@ -106,7 +106,7 @@ class TestSet1(unittest.TestCase):
 
     def test_from_base64(self):
         if "base64" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         test_input = Base64("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
         answer = Hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
         output = to_hex(from_base64(test_input))
@@ -114,7 +114,7 @@ class TestSet1(unittest.TestCase):
 
     def test_hd(self):
         if "hd" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         test_input1 = b"this is a test"
         test_input2 = b"wokka wokka!!!"
         result = hamming_distance(test_input1, test_input2)
@@ -125,7 +125,7 @@ class TestSet1(unittest.TestCase):
         This function shows various scoring results against a couple of strings. English must be positive.
         """
         if "scoring" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         test_input = {
             "english":   [
                 b"This function shows various scoring results against a couple of strings.English must be positive."
@@ -141,7 +141,7 @@ class TestSet1(unittest.TestCase):
 
     def test_scoring2(self):
         if "scoring" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         with open("materials\\longread.txt") as f:
             text = f.read().encode()
         ret = english_scoring(text)
--- set2_tests.py
+++ set2_tests.py
@@ -29,7 +29,7 @@ class TestSet2(unittest.TestCase):
 
     def test_challenge09(self):
         if 9 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         from ch09 import pkcs7_pad
 
@@ -40,7 +40,7 @@ class TestSet2(unittest.TestCase):
 
     def test_encrypt_cbc(self):
         if "cbc" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         test = b"1234567890"
         expected = b"8vSCtvqVqJ1nFiDO39gBfg=="
@@ -50,7 +50,7 @@ class TestSet2(unittest.TestCase):
 
     def test_cbc(self):
         if "cbc" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         data = b"1234567890"
         key = b"YELLOW SUBMARINE"
         iv = 16 * b"\0"
@@ -67,7 +67,7 @@ class TestSet2(unittest.TestCase):
 
     def test_decrypt_cbc(self):
         if "cbc" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         expected = b"1234567890"
         test = base64.b64decode(b"8vSCtvqVqJ1nFiDO39gBfg==")
@@ -77,7 +77,7 @@ class TestSet2(unittest.TestCase):
 
     def test_challenge10(self):
         if 10 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         key = b"YELLOW SUBMARINE"
 
@@ -92,14 +92,14 @@ class TestSet2(unittest.TestCase):
 
     def test_oracle(self):
         if 'oracle' not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = generate_bytes(randint(1, 5))
         actual = encryption_oracle_ecb_cbc(expected, randint(1, 2) == 1)
         self.assertLess(len(expected), len(actual))
 
     def test_challenge11(self):
         if 11 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         sys.setrecursionlimit(1500)  # due to recursive functions, which was probably not a good idea
         for _ in range(1000):
             expected = randint(1, 2) == 1
@@ -108,14 +108,14 @@ class TestSet2(unittest.TestCase):
 
     def test_challenge12(self):
         if 12 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = unknown_string
         actual = crack_ecb_simple(encryption_oracle_ecb)
         self.assertEqual(expected, actual[:len(expected)])
 
     def test_kv(self):
         if 'kv' not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = {'foo': 'bar', 'baz': 'qux', 'zap': 'zazzle'}
         actual = kv_parse("foo=bar&baz=qux&zap=zazzle")
         self.assertEqual(expected, actual)
@@ -126,7 +126,7 @@ class TestSet2(unittest.TestCase):
 
     def test_check_email(self):
         if 'email' not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         actual = check_email("foo@bar.com")
         expected = True
@@ -146,7 +146,7 @@ class TestSet2(unittest.TestCase):
 
     def test_profile(self):
         if 'profile' not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = "foo@bar.com"
         actual = encrypt_profile(expected)
         actual = decrypt_profile(actual)["email"]
@@ -154,7 +154,7 @@ class TestSet2(unittest.TestCase):
 
     def test_challenge13(self):
         if 13 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = "admin"
         actual = attack_profile()
         actual = decrypt_profile(actual)
@@ -162,14 +162,14 @@ class TestSet2(unittest.TestCase):
 
     def test_challenge14(self):
         if 14 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = unknown_string
         actual = crack_ecb_hard(encryption_oracle_ecb_hard)
         self.assertEqual(expected, actual[:len(expected)])
 
     def test_challenge15(self):
         if 15 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = True
         test = b"ICE ICE BABY\x04\x04\x04\x04"
         actual = pkcs7_validate(test)
@@ -187,7 +187,7 @@ class TestSet2(unittest.TestCase):
 
     def test_ch16_mint(self):
         if "pre16" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
 
         userData = b";admin=true;"
         enc = encrypt_wrapper(userData)
@@ -196,7 +196,7 @@ class TestSet2(unittest.TestCase):
 
     def test_ch16_flip(self):
         if "flip" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         expected = b"MARSHALL Qg3!"
         temp = encrypt_cbc_fix(b"B" * 16 + b"A" * 48)
 
@@ -206,20 +206,20 @@ class TestSet2(unittest.TestCase):
 
     def test_ch16_flip_exception(self):
         if "flip" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         self.assertRaises(Exception, flip_bits, b"A" * 32, b"A" * 7, 30)
         self.assertRaises(Exception, flip_bits, b"A" * 32, b"A" * 7, 10)
 
     def test_check_admin(self):
         if "check_admin" not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         data = b";;;;"
         actual = check_admin_helper(data)
         self.assertFalse(actual)
 
     def test_challenge16(self):
         if 16 not in self.tests:
-            self.skipTest("external resource not available")
+            self.skipTest("")
         temp = crack_admin()
         actual = check_admin(temp)
         self.assertTrue(actual)
